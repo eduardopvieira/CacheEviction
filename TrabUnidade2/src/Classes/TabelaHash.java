87798ef575328229000f6c55c4062b24a85923ab
eduardopvieira <eduardoopazvieira@gmail.com>
2024-09-20 13:24:57 -0300

    feat: Resize agora funciona (e outros metodos)

--- TrabUnidade2/src/Classes/TabelaHash.java
+++ TrabUnidade2/src/Classes/TabelaHash.java
@@ -1,6 +1,7 @@
 package Classes;
 
 import java.util.LinkedList;
+import java.util.Random;
 
 public class TabelaHash {
 
@@ -40,10 +41,6 @@ public class TabelaHash {
 		// se viu que já nao ta, adiciona o novo valor na lista encadeada <3
 		n++;
 		lista.add(no);
-		
-		if (fatorDeCarga() >= 0.75) {
-			redimensionar();
-		}
 	}
 
 
@@ -72,7 +69,7 @@ public class TabelaHash {
 		for(Node node : lista) {
 			if (node.getKey() == v) {
 				lista.remove(node); //remove da linkedlist
-				this.n = n--;
+				n--;
 				return node; //retorna o removido
 			}
 		}
@@ -101,39 +98,122 @@ public class TabelaHash {
 		}
 	}
 
+	void listarElementos() {
+		for (int i = 0; i < M; i++) {
+			for ( Node no : tabela[i]) {
+				printarNode(no);
+			}
+		}
+	}
+
+
 	public double fatorDeCarga() {
 		System.out.println("Valor de n: " + n);
-		System.out.println("Valor de m: " + M);
+		System.out.println("Valor de M: " + M);
 		double fator = (double) n / M;
 		System.out.println("Fator de carga atual: " + fator);
 		return fator;
 	}
 
-	public void redimensionar() {
+	void resize(boolean aumentar) {
 		
 		System.out.println("TABELA ATUAL: ");
 		imprimirTabelaHash();
 
 		System.out.println("REDIMENSIONANDO!!!!!");
-		int novoTamanho = this.M * 2;
-		LinkedList<Node>[] novaTabela = new LinkedList[novoTamanho];
-	
-		// Inicializa a nova tabela
-		for (int i = 0; i < novoTamanho; i++) {
-			novaTabela[i] = new LinkedList<>();
+		
+		LinkedList<Node>[] temp = tabela;
+		if (aumentar) {
+			M = proxPrimo(M * 2);
+		} else {
+			M = primoAnterior(M / 2);
 		}
-	
-		// Re-hash de todos os elementos da tabela atual para a nova tabela
-		for (int i = 0; i < this.M; i++) {
-			for (Node node : this.tabela[i]) {
-				int novoHash = node.getKey() % novoTamanho;
-				novaTabela[novoHash].add(node);
+		tabela = new LinkedList[M];
+		
+		for (int i = 0; i < M; i++) {
+			tabela[i] = new LinkedList<>();
+		}
+
+		for (LinkedList<Node> lista: temp) {
+			if (lista != null) {
+				for (Node no: lista) {
+					inserir(no);
+				}
 			}
 		}
-	
-		// Substitui a tabela antiga pela nova
-		this.M = novoTamanho;
-		this.tabela = novaTabela;
+
+		temp = null;
+
 	}
+
+	int proxPrimo(int numero) {
+		while (!ehPrimo(numero)) {
+			numero++;
+		}
+		return numero;
+	}
+
+	public int primoAnterior(int numero) {
+		while (numero > 2) {
+			if (ehPrimo(numero)) {
+				return numero;
+			}
+			numero--;
+		}
+		return 2; //se nao encontrar nenhum primo, retorna 2 q é o menor possivel
+	}
+	
+	boolean ehPrimo(int numero) {
+		if (numero <= 1) return false;
+		for (int i = 2; i <= Math.sqrt(numero); i++) {
+			if (numero % i == 0) return false;
+		}
+		return true;
+	}
+
+	void printarNode(Node no) { 
+		System.out.println("=================");
+		System.out.println("Código: " + no.getKey());
+		System.out.println("Nome: " + no.getOS().getNome());
+		System.out.println("Descrição: " + no.getOS().getDescricao());
+		System.out.println("Hora: " + no.getOS().getHora());
+	}
+
+
+	public Node sortearElemento() {
+        Random rand = new Random();
+        Node noSorteado = null;
+
+		//EQNUANTO O NO SORTEADO FOR NULO:
+        while (noSorteado == null) {
+
+            // PRIMEIRO PASSO: SORTEIA UM NUMERO DENTRO DO TAMANHO M DO MEU VETOR
+            int indiceTabela = rand.nextInt(M);
+            
+			LinkedList<Node> lista = tabela[indiceTabela]; // PEGA A LISTA SORTEADA
+
+            // VERIFICA SE ELA TA VAZIA
+            if (!lista.isEmpty()) {
+                //3: SE NAO ESTIVER....
+                int indiceLista = rand.nextInt(lista.size()); // SORTEIA UM NUMERO DENTRO DA LISTA
+                noSorteado = lista.get(indiceLista); // PEGA O NO SORTEADO
+            }
+			//SE ESTIVER VAZIA, NO SORTEADO É NULO E COMEÇA DNV
+        }
+
+        System.out.println("Elemento sorteado: ");
+        printarNode(noSorteado);
+        
+        // Aqui você pode adicionar o elemento sorteado à cache
+        // Exemplo:
+        // cacheSv.addCache(noSorteado);
+
+        return noSorteado;
+    }
+
+	int size() {
+		return this.n;
+	}
+	
 
 }
