d6fddf010d5c4f5a3be9d86d562b04dadddb0508
eduardopvieira <eduardoopazvieira@gmail.com>
2024-09-20 13:22:59 -0300

    refactor: Simplificando a main

--- TrabUnidade2/src/Main.java
+++ TrabUnidade2/src/Main.java
@@ -1,39 +1,17 @@
 import Classes.Cliente;
 import Classes.Node;
 import Classes.OS;
+import Classes.Servidor;
 import Classes.TabelaHash;
 import Exception.MyException;
 
 public class Main {
     public static void main(String[] args) throws MyException {
 
-        TabelaHash baseDados = new TabelaHash(2);
-        TabelaHash cache = new TabelaHash(20);
-        //Cliente cli = new Cliente(baseDados);
+        Servidor baseDados = new Servidor(2);
+        Cliente cli = new Cliente(baseDados);
 
-        // Loop para criar e inserir 60 dados
-        for (int i = 1; i <= 70; i++) {
-            // Cria um nome de operação fictício
-            String nome = "Operação " + i;
-
-            // Cria uma descrição fictícia
-            String descricao = "Descrição da operação " + i;
-
-            // Cria um horário fictício (pode ser ajustado conforme necessário)
-            String horario = String.format("%02d:%02d", (i % 24), (i % 60));
-
-            // Cria uma instância de OS com os dados gerados
-            OS os = new OS(nome, descricao, horario);
-
-            // Cria uma instância de Node com um código incremental
-            Node node = new Node(i, os);
-
-            // Insere o nó na base de dados
-            baseDados.inserir(node);
-
-        }
-
-        baseDados.imprimirTabelaHash();
+        cli.comecarSimulacao();
 
 
     }
