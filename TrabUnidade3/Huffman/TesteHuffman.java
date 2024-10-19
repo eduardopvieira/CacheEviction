8e650424a0c068aebbdaaa0640a5b15d972a958f
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-19 17:30:29 -0300

    teste para provar que a arvore de huffman funciona

---
+++ TrabUnidade3/Huffman/TesteHuffman.java
@@ -0,0 +1,14 @@
+package Huffman;
+
+class TesteHuffman {
+    
+    public static void main(String[] args) {
+        int n = 6;
+        char[] vetorDeCaracteres = { 'a', 'b', 'c', 'd', 'e', 'f' };
+        int[] vetorDeFrequencias = { 5, 9, 12, 13, 16, 45 };
+        ArvoreHuffman arv = new ArvoreHuffman();
+        arv.construirArvore(n, vetorDeCaracteres, vetorDeFrequencias);
+        arv.imprimirCodigo(arv.raiz, "");
+    }
+}
+
