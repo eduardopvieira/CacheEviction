38dabb9baff053cf5751eab4227d77a35bd1b4ea
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-20 17:56:31 -0300

    criaçao do metodo de inserir no servidor

--- TrabUnidade3/Classes/Servidor.java
+++ TrabUnidade3/Classes/Servidor.java
@@ -1,5 +1,6 @@
 package Classes;
 
+import Huffman.ArvoreHuffman;
 
 public class Servidor {
     
@@ -15,7 +16,34 @@ public class Servidor {
     }
 
 
-    public void inserir(Node no) {
+    public void inserir(String msgCodificada, ArvoreHuffman arvh) {
+
+        String decodificada = arvh.descomprimir(msgCodificada);
+
+        System.out.println("MENSAGEM DECODIFICADA: " + decodificada);
+
+        String[] partes = decodificada.split(" ");
+
+        int chave = -1;
+        String nome = "";
+        String descricao = "";
+        String hora = "";
+
+        for (int i = 0; i < partes.length; i++) {
+            if (partes[i].equals("Chave:")) {
+                chave = Integer.parseInt(partes[i + 1]);
+            } else if (partes[i].equals("Nome:")) {
+                nome = partes[i + 1];
+            } else if (partes[i].equals("Descricao:")) {
+                descricao = partes[i + 1];
+            } else if (partes[i].equals("Hora:")) {
+                hora = partes[i + 1];
+            }
+
+        }
+        OS novo = new OS(nome, descricao, hora);
+        Node no = new Node(chave, novo);
+
         tabela.inserir(no);	
 		if (tabela.fatorDeCarga() >= 7.5) { // *"Sedgewick recomenda escolher m tal que n/m fique entre 5 e 10."
 			tabela.resize(true);
