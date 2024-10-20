0d3b282c43f8980e5279d274ca78442581b53846
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-20 17:55:24 -0300

    criacao de uma main

---
+++ TrabUnidade3/Main.java
@@ -0,0 +1,14 @@
+import Classes.Cliente;
+import Classes.Servidor;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Servidor baseDados = new Servidor(2);
+        Cliente cli = new Cliente(baseDados);
+
+        cli.comecarSimulacao();
+
+
+    }
+}
