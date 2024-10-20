899f37267c3b99cea29ccac8075b599a758b200f
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-20 17:57:39 -0300

    alteracao pequena no teste da arvore de huffman

--- TrabUnidade3/Testes/TesteHuffman.java
+++ TrabUnidade3/Testes/TesteHuffman.java
@@ -5,11 +5,10 @@ import Huffman.ArvoreHuffman;
 class TesteHuffman {
     
     public static void main(String[] args) {
-        int n = 6;
         char[] vetorDeCaracteres = { 'a', 'b', 'c', 'd', 'e', 'f' };
         int[] vetorDeFrequencias = { 5, 9, 12, 13, 16, 45 };
         ArvoreHuffman arv = new ArvoreHuffman();
-        arv.construirArvore(n, vetorDeCaracteres, vetorDeFrequencias);
+        arv.construirArvore(vetorDeCaracteres, vetorDeFrequencias);
         arv.imprimirCodigo(arv.raiz, "");
     }
 }
