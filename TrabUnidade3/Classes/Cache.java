3122ede839923296fac0699cc6068ed4cec545b6
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-21 19:12:27 -0300

    atualizaçao enorme na cache

--- TrabUnidade3/Classes/Cache.java
+++ TrabUnidade3/Classes/Cache.java
@@ -1,11 +1,51 @@
 package Classes;
 
+import Huffman.ArvoreHuffman;
+
 public class Cache {
     
     private ListaAutoAjustavel cache = new ListaAutoAjustavel(30);
 
     public void adicionar (Node add) {
-        cache.inserir(add.getKey(), add.getOS());
+        cache.inserir(add);
+    }
+
+    public Node buscarCache(int indice) {
+        return cache.buscar(indice);
+    }
+
+    public Node removerCache(int indice) {
+        return cache.remover(indice);
+    }
+
+    public void printarCache() {
+        cache.imprimir();
+    }
+
+    public ListaAutoAjustavel getCache() {
+        return this.cache;
+    }
+
+    public DadosCompressao buscaComprimidaCache (int indice) {
+       
+        Node result = cache.buscar(indice);
+
+        if (result == null) {
+            System.out.println("Indice do OS nao encontrado para comprimir.");
+            return null;
+        }
+
+        ArvoreHuffman arvh = new ArvoreHuffman();
+        String msg = result.gerarMensagem();
+        char[] vetorChar = new char[msg.length()];
+        int[] vetorFreq = new int[msg.length()];
+
+        arvh.contarCaractereFrequencia(msg, vetorChar, vetorFreq);
+        arvh.construirArvore(vetorChar, vetorFreq);
+        String comprimido = arvh.comprimir(msg);
+        
+        DadosCompressao dc = new DadosCompressao(arvh, comprimido);
+        return dc;
     }
    
 }
