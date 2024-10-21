4aa27d61513a94a2551afd751d5e810b0592ad5b
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-21 19:12:37 -0300

    atualizacao em metodos do cliente

--- TrabUnidade3/Classes/Cliente.java
+++ TrabUnidade3/Classes/Cliente.java
@@ -1,6 +1,5 @@
 package Classes;
 
-
 import java.util.Scanner;
 
 import Huffman.ArvoreHuffman;
@@ -9,7 +8,6 @@ public class Cliente {
 
     Servidor sv;
     Cache cacheSv = new Cache();
-    
 
     public Cliente() {
     }
@@ -18,9 +16,9 @@ public class Cliente {
         this.sv = sv;
     }
 
-    public void comecarSimulacao()  {
-        //Logger logger = new Logger(this.sv, cacheSv);
-        //inicializacao();
+    public void comecarSimulacao() {
+        Logger logger = new Logger(this.sv, cacheSv);
+        inicializacao();
 
         int decisao = 0;
         while (decisao != 9) {
@@ -33,7 +31,8 @@ public class Cliente {
             System.out.println("[5] - Quantidade de registros");
             System.out.println("[6] - Buscar OS");
             System.out.println("[7] - Printar cache");
-            System.out.println("[8] - Sair");
+            System.out.println("[8] - Printar base de dados");
+            System.out.println("[9] - Sair");
             System.out.println("Decisão: ");
             Scanner sc = new Scanner(System.in);
             decisao = sc.nextInt();
@@ -46,22 +45,23 @@ public class Cliente {
                     int codigo = sc.nextInt();
                     sc.nextLine();
 
-                    if (true) {//(buscarCacheSv(codigo) == null) {
+                    if (buscarCacheSv(codigo) == null) {
 
-                        //se nao achou na cache nem na base de dados, começa o cadastro
-    
+                        // se nao achou na cache nem na base de dados, começa o cadastro
+
                         System.out.println("Digite o nome da operação: ");
                         String nome = sc.nextLine();
-    
+
                         System.out.println("Digite a descrição: ");
                         String desc = sc.nextLine();
-    
+
                         System.out.println("Digite o horário: ");
                         String hora = sc.nextLine();
-    
+
                         OS os = new OS(nome, desc, hora);
                         Node no = new Node(codigo, os);
-    
+
+                        //! parte da compressao
                         System.out.println("Comprimindo OS:");
                         ArvoreHuffman arvh = new ArvoreHuffman();
                         String msg = no.gerarMensagem();
@@ -69,139 +69,141 @@ public class Cliente {
                         char[] arrayChar = new char[msg.length()];
                         arvh.contarCaractereFrequencia(msg, arrayChar, arrayNums);
                         arvh.construirArvore(arrayChar, arrayNums);
-                        String msgCodificada = arvh.codificar(msg);
-                        
+                        String msgCodificada = arvh.comprimir(msg);
+
                         sv.inserir(msgCodificada, arvh);
 
                         System.out.println("OS cadastrado com sucesso.");
+                        logger.log(no);
                     } else {
                         System.out.println("O código digitado já existe na base de dados.");
                     }
-                    //logger.log();
                     break;
-                    
-                    
+
                 case 2:
                     System.out.println("Listando todos os OS: ");
                     sv.tabela.listarElementos();
-                    //logger.log();
+                    logger.log(null);
                     break;
 
-                // case 3:
-                //     System.out.println("Digite o código do OS a ser alterado: ");
-                //     int cod = sc.nextInt();
-                //     Node alter = buscarCacheSv(cod);
-                //     if (alter != null) {
+                case 3:
+                    System.out.println("Digite o código do OS a ser alterado: ");
+                    int cod = sc.nextInt();
 
-                //         System.out.println("Qual parâmetro você quer alterar?");
-                //         System.out.println("1 - Nome");
-                //         System.out.println("2 - Descrição");
-                //         System.out.println("3 - Horário");
-                //         System.out.println("4 - Todos");
-                //         int choice = sc.nextInt();
-                //         sc.nextLine();
+                    //! descompressão aqui
+                    Node alter = buscarCacheSv(cod);
+                    
+                    if (alter != null) {
 
-                //         String newNome = alter.getOS().getNome();
-                //         String newDesc = alter.getOS().getDescricao();
-                //         String newHora = alter.getOS().getHora();
+                        System.out.println("Qual parâmetro você quer alterar?");
+                        System.out.println("1 - Nome");
+                        System.out.println("2 - Descrição");
+                        System.out.println("3 - Horário");
+                        System.out.println("4 - Todos");
+                        int choice = sc.nextInt();
+                        sc.nextLine();
 
-                //         switch (choice) {
-                //             case 1:
-                //                 System.out.println("Digite o novo nome da operação:");
-                //                 newNome = sc.nextLine();
-                //                 break;
+                        String newNome = alter.getOS().getNome();
+                        String newDesc = alter.getOS().getDescricao();
+                        String newHora = alter.getOS().getHora();
 
-                //             case 2:
-                //                 System.out.println("Digite a nova descrição:");
-                //                 newDesc = sc.nextLine();
-                //                 break;
-                //             case 3:
-                //                 System.out.println("Digite o novo horário:");
-                //                 newHora = sc.nextLine();
-                //                 break;
-                //             case 4:
+                        switch (choice) {
+                            case 1:
+                                System.out.println("Digite o novo nome da operação:");
+                                newNome = sc.nextLine();
+                                break;
 
-                //                 System.out.println("Digite o novo nome da operação:");
-                //                 newNome = sc.nextLine();
+                            case 2:
+                                System.out.println("Digite a nova descrição:");
+                                newDesc = sc.nextLine();
+                                break;
+                            case 3:
+                                System.out.println("Digite o novo horário:");
+                                newHora = sc.nextLine();
+                                break;
+                            case 4:
 
-                //                 System.out.println("Digite a nova descrição:");
-                //                 newDesc = sc.nextLine();
+                                System.out.println("Digite o novo nome da operação:");
+                                newNome = sc.nextLine();
 
-                //                 System.out.println("Digite o novo horário:");
-                //                 newHora = sc.nextLine();
-                //                 break;
+                                System.out.println("Digite a nova descrição:");
+                                newDesc = sc.nextLine();
 
-                //             default:
-                //                 System.out.println("Opção inválida. Nenhuma alteração realizada.");
-                //                 return;
-                //         }
+                                System.out.println("Digite o novo horário:");
+                                newHora = sc.nextLine();
+                                break;
 
-                //         OS newOs = new OS(newNome, newDesc, newHora);
-                //         alter.setOS(newOs);
-                //         System.out.println("Alteração realizada com sucesso.");
-                //         logger.log();
-                //         break;
-                //     }
-                //     System.out.println("Nenhum OS encontrado com esse código.");
-                //     break;
+                            default:
+                                System.out.println("Opção inválida. Nenhuma alteração realizada.");
+                                return;
+                        }
 
+                        OS newOs = new OS(newNome, newDesc, newHora);
+                        alter.setOS(newOs);
+                        System.out.println("Alteração realizada com sucesso.");
+                        //logger.log();
+                        break;
+                    }
+                    System.out.println("Nenhum OS encontrado com esse código.");
+                    break;
 
-                // case 4:
-                //     System.out.println("Digite o código do OS a ser removido: ");
-                //     int remov = sc.nextInt();
-                //     sc.nextLine();
-                //     Node removido = sv.remover(remov);
+                case 4:
+                    System.out.println("Digite o código do OS a ser removido: ");
+                    Integer remov = sc.nextInt();
+                    sc.nextLine();
 
-                //     if (removido != null) {
-                //         // tirando da hash
-                //         sv.remover(remov);
+                    //! compressao acontece aqui ------V
+                    DadosCompressao removido = sv.removerComprimido(remov);
+                    
+                    if (removido != null) {
+                        
+                        Node nodeRemovido = removido.descomprimir();
+                        
+                        sv.remover(nodeRemovido.getKey());
 
-                //         // removendo da cache usando o codigo do node
-                //         Node temp = cacheSv.removeCache(remov);
-                //         if (temp != null) {
-                //             adicionarRandomCache();
-                //         }
+                        cacheSv.removerCache(nodeRemovido.getKey());
 
-                //     } else {
-                //         System.out.println("Nenhum OS encontrado com esse código.");
-                //     }
-                //     logger.log();
-                //     break;
+                    } else {
+                        System.out.println("Nenhum OS encontrado com esse código.");
+                    }
+                    // logger.log();
+                    break;
 
-                // case 5:
-                //     System.out.println("Quantidade de registros: ");
-                //     System.out.println(sv.tabela.n);
-                //     break;
+                case 5:
+                    System.out.println("Quantidade de registros: ");
+                    System.out.println(sv.tabela.n);
+                    break;
 
-                // case 6:
+                case 6:
 
-                //     System.out.println("Digite o código do OS que deseja buscar:");
-                //     int buscar = sc.nextInt();
-                //     sc.nextLine();
+                    System.out.println("Digite o código do OS que deseja buscar:");
+                    int buscar = sc.nextInt();
+                    sc.nextLine();
 
-                //     Node retorno = printarCacheSv(buscar);
-                //     if (retorno != null) {
-                //         cacheSv.addCache(retorno);
-                //     }
+                    //! descompressao nesse metodo:
+                    Node retorno = buscarCacheSv(buscar);
+                    
+                    if (retorno != null) {
+                        cacheSv.adicionar(retorno);
+                    }
 
-                //     logger.log();
+                    logger.log(null);
 
-                //     break;
+                    break;
 
-                // case 7:
-                //     System.out.println("Printando fator de carga:");
-                //     sv.tabela.fatorDeCarga();
-                //     break;
+                case 7:
+                    System.out.println("Cache:");
+                    cacheSv.printarCache();
+                    break;
 
-                // case 8:
-                //     System.out.println("Cache:");
-                //     cacheSv.printarCache();
-                //     break;
-                
+                case 8:
+                    System.out.println("Base de dados:");
+                    sv.printarServer();
+                    break;
+
                 case 9:
                     System.out.println("Encerrando programa.");
                     break;
-
 
                 default:
                     System.out.println("Número nao reconhecido. Tente novamente.");
@@ -210,58 +212,98 @@ public class Cliente {
         }
     }
 
-    // Node buscarCacheSv(int x) {
-    //     Node retorno = cacheSv.buscarCache(x);
+    Node buscarCacheSv(int x) {
         
-    //     if (retorno == null) { //nao achou na cache
-    //         System.out.println("Nada na cache. Resposta do servidor:");
-    //         return sv.buscar(x); //vai retornar a resposta do servidor
-    //     }
-    //     System.out.println("Encontrado na cache:");
-    //     return retorno; //achou na cache
+        //! recebe o node comprimido nesse metodo ---------v
+        DadosCompressao nodeCacheComprimido = cacheSv.buscaComprimidaCache(x);
         
-    // }
+        if (nodeCacheComprimido == null) { // ? nao encontrou na cache
 
-    // Node printarCacheSv(int x) {
+            System.out.println("Nada na cache. Resposta do servidor:");
+            DadosCompressao nodeSvComprimido = sv.buscaComprimidaServer(x);
+
+            if (nodeSvComprimido == null) { // ? nao encontrou nada no servidor ou cache
+
+                System.out.println("Node nao encontrado no servidor.");
+                return null;
+            }
+
+            //? encontrou no servidor
+
+            Node retorno = nodeSvComprimido.descomprimir();
+            System.out.println("ENCONTRADO NO SERVIDOR");
+            System.out.println("NODE COMPRIMIDO:" + nodeSvComprimido.msgComprimida);
+            System.out.println("NODE DESCOMPRIMIDO: " + retorno.gerarMensagem() );
+            return retorno;
+
+        }
+
+        //? encontrou na cache
+
+        Node retorno = nodeCacheComprimido.descomprimir();
         
-    //     Node no = cacheSv.buscarCache(x);
         
-    //     if (no == null) { //nao achou na cache
-            
-    //         no = sv.buscar(x);
-            
-    //         if (no == null) {
-    //             System.out.println("Código não encontrado.");
-    //         } else {
-    //             System.out.println("Encontrado na base de dados:");
-    //             sv.tabela.printarNode(no);    
-    //         }
-
-    //     } else {
-    //         System.out.println("Encontrado na cache:");
-    //         sv.tabela.printarNode(no);
-    //     }
-        
-    //     return no;
-    // }
+        System.out.println("Encontrado na cache:");
+        System.out.println("NODE COMPRIMIDO:" + nodeCacheComprimido.msgComprimida);
+        System.out.println("NODE DESCOMPRIMIDO: " + retorno.gerarMensagem() );
 
 
-    // private void inicializacao() {
-    //      for (int i = 1; i <= 70; i++) {
-    //         String nome = "Operação " + i;
-    //         String descricao = "Descrição da operação " + i;
-    //         String horario = String.format("%02d:%02d", (i % 24), (i % 60));
-    //         OS os = new OS(nome, descricao, horario);
-    //         Node node = new Node(i, os);
-    //         sv.inserir(node);
+        return retorno;
 
-    // }
+    }
 
-    //     Node add;
-    //     for (int x = 1; x <= 20; x++) {
-    //         add = sv.buscar(x);
-    //         cacheSv.addCache(add);
-    //     }
-    // }
+    Node printarCacheSv(int x) {
 
+        Node no = cacheSv.buscarCache(x);
+
+        if (no == null) { // nao achou na cache
+
+            no = sv.buscar(x);
+
+            if (no == null) {
+                System.out.println("Código não encontrado.");
+            } else {
+                System.out.println("Encontrado na base de dados:");
+                sv.tabela.printarNode(no);
+            }
+
+        } else {
+            System.out.println("Encontrado na cache:");
+            sv.tabela.printarNode(no);
+        }
+
+        return no;
+    }
+    
+
+    private void inicializacao() {
+        for (int i = 1; i <= 70; i++) {
+            String nome = "Operação" + i;
+            String descricao = "Desc" + i;
+            String horario = String.format("%02d:%02d", (i % 24), (i % 60));
+            OS os = new OS(nome, descricao, horario);
+            Node node = new Node(i, os);
+            sv.inserirInicializacao(node);
+        }
+        sv.printarServer();
+
+        Node add;
+        int cont = 0;
+        int x = 1;
+        while (cont < 30) {
+            System.out.println("VALOR PROCURADO: " + x);
+            add = sv.buscar(x);
+            if (add != null) {
+                System.out.println("ACHADO PRA ADICIONAR NA CACHE: " + add.gerarMensagem());
+                cacheSv.adicionar(add);
+                cont++;
+                System.out.println("CONTADOR: " + cont);
+                cacheSv.printarCache();
+            } else {
+                System.out.println("achou um nulo");
+                System.out.println(cont);
+            }
+            x++;
+        }
+    }
 }
