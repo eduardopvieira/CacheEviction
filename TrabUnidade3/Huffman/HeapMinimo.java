e0c24affe3caf9d35aab8163e6095de3237e3d3b
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-23 12:38:40 -0300

    refactor heap minimo

--- TrabUnidade3/Huffman/HeapMinimo.java
+++ TrabUnidade3/Huffman/HeapMinimo.java
@@ -11,19 +11,7 @@ public class HeapMinimo {
         this.heap = new NodeHuffman[n];
         this.tamanho = 0;
     }
-
-    private int indicePai(int i) {
-        return (i - 1) / 2;
-    }
-
-    private int indiceFilhoEsquerdo(int i) {
-        return 2 * i + 1;
-    }
-
-    private int indiceFilhoDireito(int i) {
-        return 2 * i + 2;
-    }
-
+    
     public void inserir(NodeHuffman node) {
         if (tamanho == capacidade) {
             throw new IllegalStateException("Heap cheio");
@@ -41,7 +29,8 @@ public class HeapMinimo {
 
     public NodeHuffman removerMinimo() {
         if (tamanho == 0) {
-            throw new IllegalStateException("Heap vazio");
+            System.out.println("Lista vazia. Nao da pra remover nada.");
+            return null;
         }
 
         NodeHuffman raiz = heap[0];
@@ -54,37 +43,44 @@ public class HeapMinimo {
         return raiz;
     }
 
-    // Método para reorganizar o heap, "descendo" o nó no índice dado até a posição correta
     private void heapify(int i) {
-        int menor = i;  // Inicialmente, assumimos que o menor nó é o do índice atual
-        int esquerdo = indiceFilhoEsquerdo(i);  // Índice do filho esquerdo
-        int direito = indiceFilhoDireito(i);  // Índice do filho direito
+        int menor = i;
+        int esquerdo = indiceFilhoEsquerdo(i);
+        int direito = indiceFilhoDireito(i);
 
-        // filho esquerdo tem uma frequência menor que o nó atual?
         if (esquerdo < tamanho && heap[esquerdo].freq < heap[menor].freq) {
             menor = esquerdo;
         }
 
-        // filho direito tem uma frequência menor que o menor valor encontrado?
         if (direito < tamanho && heap[direito].freq < heap[menor].freq) {
             menor = direito;
         }
 
-        // se o menor no n estiver no indice atual, troca os nos e continua a organizar
         if (menor != i) {
             trocar(i, menor);
             heapify(menor);  // Continua organizando o heap recursivamente
         }
     }
 
-    // Método para trocar dois nós no heap
+    private int indicePai(int i) {
+        return (i - 1) / 2;
+    }
+
+    private int indiceFilhoEsquerdo(int i) {
+        return 2 * i + 1;
+    }
+
+    private int indiceFilhoDireito(int i) {
+        return 2 * i + 2;
+    }
+
+
     private void trocar(int i, int j) {
         NodeHuffman temp = heap[i];
         heap[i] = heap[j];
         heap[j] = temp;
     }
 
-    // Método para exibir o heap (apenas para visualização)
     public void imprimirHeap() {
         for (int i = 0; i < tamanho; i++) {
             System.out.println(heap[i]);
