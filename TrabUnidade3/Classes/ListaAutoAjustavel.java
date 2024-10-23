db5851031f394be2bc8add43a2d561dcdd518ac5
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-23 12:31:08 -0300

    correção de bug na lista auto ajustavel

--- TrabUnidade3/Classes/ListaAutoAjustavel.java
+++ TrabUnidade3/Classes/ListaAutoAjustavel.java
@@ -6,6 +6,8 @@ public class ListaAutoAjustavel {
     private int tam;       // tamanho max
     private int tamAtual;  // qtd de nodes atual
 
+    //? é uma LAA estática com transposição (anotando aqui pra nao dar um branco no video)
+
     public ListaAutoAjustavel(int t) {
         tam = t;
         lista = new Node[tam];
@@ -28,29 +30,21 @@ public class ListaAutoAjustavel {
             lista[tamAtual] = no;
             tamAtual++;
         } else {
-            // Lista cheia, remover o último e inserir o novo node
             System.out.println("Lista cheia, substituindo o último valor (chave " + lista[tam - 1].getKey() + ") por " + no.getKey());
-            removerUltimo();  // remove o último node da lista
+            removerUltimo();
             lista[tamAtual] = no;
-            tamAtual++;
+            tamAtual++; //tem q ter esse tamAtual++ pq o remover ultimo decrementa ele
         }
-    }
-
-    private boolean existe(int ch) {
-        for (int i = 0; i < tamAtual; i++) {
-            if (lista[i].getKey() == ch) {
-                transpor(i);  // Se o elemento já existe, faz a transposição
-                return true;
-            }
-        }
-        return false;
     }
 
     private void transpor(int index) {
         if (index > 0) {
+            imprimir();
+            
             Node temp = lista[index];
             lista[index] = lista[index - 1];
             lista[index - 1] = temp;
+
         }
     }
 
@@ -67,11 +61,10 @@ public class ListaAutoAjustavel {
         if (index != -1) {
             Node retorno = lista[index];
             for (int i = index; i < tamAtual - 1; i++) {
-                lista[i] = lista[i + 1];  // move o resto dos elementos pra preencher espaço
+                lista[i] = lista[i + 1];
             }
-            lista[tamAtual - 1] = null;  // limpando o último elemento
+            lista[tamAtual - 1] = null;
             tamAtual--;
-            System.out.println("Elemento de chave " + ch + " removido da lista auto ajustável.");
             return retorno;
         } else {
             System.out.println("Chave não encontrada na cache.");
@@ -81,7 +74,6 @@ public class ListaAutoAjustavel {
 
     private void removerUltimo() {
         if (tamAtual > 0) {
-            System.out.println("Removendo o último elemento: " + lista[tamAtual - 1].getKey());
             lista[tamAtual - 1] = null;
             tamAtual--;
         }
@@ -91,12 +83,21 @@ public class ListaAutoAjustavel {
         for (int i = 0; i < tamAtual; i++) {            
             if (lista[i].getKey() == ch) {
                 Node retorno = lista[i];
-                transpor(i);  // Ao buscar, transpõe o elemento encontrado
+                transpor(i);
                 return retorno;
             }
         }
-        System.out.println("Chave " + ch + " não encontrada na lista.");
+        System.out.println("Chave " + ch + " não encontrada na CACHE.");
         return null;
+    }
+
+    public boolean existe(int ch) {
+        for (int i = 0; i < tamAtual; i++) {            
+            if (lista[i].getKey() == ch) {
+                return true;
+            }
+        }
+        return false;
     }
     
 
@@ -113,7 +114,7 @@ public class ListaAutoAjustavel {
 
     public void imprimir() {
         for (int i = 0; i < tamAtual; i++) {
-            System.out.print(lista[i].getKey() + " ");
+            System.out.print("[" + lista[i].getKey() +"] ");
         }
         System.out.println();
     }
